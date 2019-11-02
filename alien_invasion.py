bda0abd415010e4162ccc9adad8d7db2709217cd
Kiryl <kbartashevich@carecloud.com>
2019-11-01 21:49:06 -0400

    Added ship to game

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
 	"""Overall class to manage game assets and behavior."""
@@ -14,6 +15,8 @@ class AlienInvasion:
 		self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 		pygame.display.set_caption("Alien Invasion")
 
+		self.ship = Ship(self)
+
 		# Set the background color.
 
 		self.bg_color = (self.settings.bg_color)
@@ -23,18 +26,29 @@ class AlienInvasion:
 
 		while True:
 			# Watch for keyboard and mouse events.
-
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					sys.exit()
+			self._check_events()
+			
 
 			# Redraw the screen during each pass through the loop.
+			self._update_screen()
 			
-			self.screen.fill(self.bg_color)		
 
-			# Make the most recently drawn screen visible.
+	def _check_events(self):
+		"""Respond to keypresses and mouse events."""
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()	
+
+	def _update_screen(self):
+		"""Update images on the screen, and flip to the new screen."""
+
+		self.screen.fill(self.bg_color)
+		self.ship.blitme()		
+
+		# Make the most recently drawn screen visible.
 			
-			pygame.display.flip()
+		pygame.display.flip()			
 
 if __name__ == '__main__':
 	# Make a game instance, and run the game.
